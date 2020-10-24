0ae89ed021a4266750f159c784904294acd2d481
himmetgencer <himmetgencer@gmail.com>
2020-10-24 17:39:53 +0300

    Add PinDetect Lib and hour,minute functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "PinDefinitions.h"
 #include "nixie.h"
+#include "PinDetect.h"
 
 DigitalOut status(statusLed);
 
@@ -10,6 +11,48 @@ NIXIE digit3(DIGIT3_PINA, DIGIT3_PINB, DIGIT3_PINC, DIGIT3_PIND);
 NIXIE digit4(DIGIT4_PINA, DIGIT4_PINB, DIGIT4_PINC, DIGIT4_PIND);
 
 Clock_Digit digit;
+
+PinDetect hourButton(HOUR_BUTTON_PIN);
+PinDetect minuteButton(MINUTE_BUTTON_PIN);
+
+bool hourButtonState = false;
+bool minuteButtonState = false;
+
+void hourButtonPressed(void){
+    hourButtonState = true;
+}
+
+void hourButtonReleased(void){
+    hourButtonState = false;
+}
+
+void minuteButtonPressed(void){
+    minuteButtonState = true;
+}
+
+void minuteButtonReleased(void){
+    minuteButtonState = false;
+}
+
+void incrementHour(void){
+    time_t now = time(NULL);
+    struct tm *now_tm = localtime(&now);
+
+    now_tm->tm_hour = (now_tm->tm_hour +1) % 24; //increment hour
+    now_tm->tm_sec = 0;
+
+    set_time(mktime(now_tm));
+}
+
+void incrementMinute(void){
+    time_t now = time(NULL);
+    struct tm *now_tm = localtime(&now);
+
+    now_tm->tm_min = (now_tm->tm_min +1) % 60; //increment minute
+    now_tm->tm_sec = 0;
+
+    set_time(mktime(now_tm));
+}
 
 int main()
 {
@@ -22,6 +65,15 @@ int main()
         digit2.setDigit(digit.hour_digit2);
         digit3.setDigit(digit.minute_digit1);
         digit4.setDigit(digit.minute_digit2);
+
+        if(hourButtonState){
+            incrementHour();
+        }
+
+        if(minuteButtonState){
+            incrementMinute();
+        }
+
         status = !status;
         ThisThread::sleep_for(250);
 	
